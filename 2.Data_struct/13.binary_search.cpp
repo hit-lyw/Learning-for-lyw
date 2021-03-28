41c89e7618d073d399dac7a2ce53cfdde170a43f
jialonglaoba <1643635194@qq.com>
2021-03-28 18:22:24 +0800

    hashtable

---
+++ 2.Data_struct/13.binary_search.cpp
@@ -0,0 +1,55 @@
+/*************************************************************************
+	> File Name: 13.binary_search.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: 2021年03月28日 星期日 10时48分34秒
+ ************************************************************************/
+#include <stdio.h>
+#include <time.h>
+
+#define P(func, args...) {\
+    printf("%s = %d\n", #func, func(args));\
+}
+
+int binary_search1(int *num, int n, int x) {
+    int head = 0, tail = n - 1, mid;
+    while (head <= tail) {
+        mid = (head + tail) >> 1;
+        if (num[mid] == x) return mid;
+        if (num[mid] < x) head = mid + 1;
+        else tail = mid - 1;
+    }
+    return -1;
+}
+
+//1111111000000
+int binary_search2(int *num, int n) {
+    int head = -1, tail = n - 1, mid;
+    while (head < tail) {
+        mid = (head + tail + 1) >> 1;
+        if (num[mid] == 1) head = mid;
+        else tail = mid - 1;
+    }
+    return head;
+}
+
+int binary_search3(int *num, int n) {
+    int head = 0, tail = n, mid;
+    while (head < tail) {
+        mid = head + (tail - head >> 1);
+        if (num[mid] == 1) tail = mid;
+        else head = mid + 1;
+    }
+    return head == n ? -1 : head;
+}
+
+int main() {
+    int arr1[10] = {1, 3, 5, 7, 9, 11, 13, 17, 19, 21};
+    int arr2[10] = {1, 1, 1, 1, 0, 0, 0, 0, 0, 0};
+    int arr3[10] = {0, 0, 0, 0, 0, 0, 1, 1, 1, 1};
+    P(binary_search1, arr1, 10, 7);
+    P(binary_search2, arr2, 10);
+    P(binary_search3, arr3, 10);
+
+    return 0;
+}
